eac0fee89e7af1f5507ac2fb85fb2f2f4c97c79b
manzhaimaksim <maxbbb88@gmail.com>
2022-10-02 13:05:41 +0300

    add methods max and replace for Arrays

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -1,8 +1,12 @@
 module Exercise
   module Arrays
     class << self
+      def max(array)
+        array.reduce(array[0]) { |max_value, element| element > max_value ? element : max_value }
+      end
+
       def replace(array)
-        array
+        array.map { |element| element.positive? ? max(array) : element }
       end
 
       def search(_array, _query)
