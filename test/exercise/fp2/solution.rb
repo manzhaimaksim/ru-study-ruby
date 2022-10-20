1291115429ac5824f2014280f559a41013dd9968
manzhaimaksim <maxbbb88@gmail.com>
2022-10-20 14:12:44 +0300

    add method my_each

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,7 +5,14 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each; end
+      def my_each
+        return to_enum unless block_given?
+        return [] if empty?
+
+        for item in self
+          yield(item)
+        end
+      end
 
       # Написать свою функцию my_map
       def my_map; end
